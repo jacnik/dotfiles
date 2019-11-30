a816e3faf28404735ea44d0e29145ede68ee8a3c
Jacek Kubicki <jacnik@gmail.com>
2019-11-30 08:31:33 +0100

    commented apt-get install's from source_links.sh

--- source_links.sh
+++ source_links.sh
@@ -1,10 +1,10 @@
 # install necessary software
-sudo apt-get update
-sudo apt-get install && \
-	git 
-	curl
-	vim
-	zsh
+# sudo apt-get update
+# sudo apt-get install && \
+#	git 
+#	curl
+#	vim
+#	zsh
 	
 
 
@@ -18,6 +18,7 @@ source_file () {
     if [ ! \( -e "${destination}" \) ]
     then
         ln -s ${PWD}/${filename} ${destination}
+	echo "created link: ${destination}."
     else
         echo "file ${destination} already exists."
     fi
@@ -27,18 +28,19 @@ source_file ".vimrc"
 source_file ".gitconfig"
 source_file ".oh-my-zsh"
 source_file ".zshrc"
+source_file ".condarc"
 
 
 # ranger file manager:
-sudo apt install -y  highlight atool w3m poppler-utils caca-utils w3m-img
-git clone https://github.com/hut/ranger.git
-cd ranger
-sudo make install
-cd ..
-sudo rm -r ranger
-ranger --copy-config=all
+# sudo apt install -y  highlight atool w3m poppler-utils caca-utils w3m-img
+# git clone https://github.com/hut/ranger.git
+# cd ranger
+# sudo make install
+# cd ..
+# sudo rm -r ranger
+# ranger --copy-config=all
 
 # htop process viewer
-sudo apt-get install htop
+# sudo apt-get install htop
 
 
